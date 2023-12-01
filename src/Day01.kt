e9c235792daf55218a697db2972fb7d474c1acd1
Taylor Misch <taylor.misch@datasite.com>
2023-12-01 16:20:36 -0600

    2023-12-01 correct answer

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+        val numbers = mutableListOf<Int>()
+        for (line in input) {
+            var firstNumber = ""
+            var lastNumber = ""
+            for (character in line) {
+                if (character.isDigit()) {
+                    if (firstNumber == "") {
+                        firstNumber = character.toString()
+                    }
+                    lastNumber = character.toString()
+                }
+            }
+            val number = firstNumber + lastNumber
+            numbers.add(number.toInt())
+        }
+
+        var runningTotal = 0
+        for (number in numbers) {
+            runningTotal += number
+        }
+
+        return runningTotal
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
 }

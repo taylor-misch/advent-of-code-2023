fa00480c1dd016632dea38d9a1f9042d73d16262
Taylor Misch <taylor.misch@datasite.com>
2023-12-09 09:18:53 -0600

    Day 4 problem completed

--- src/Day04.kt
+++ src/Day04.kt
@@ -6,7 +6,9 @@ fun main() {
         val cardNumber: Int,
         val winningNumbers: List<Int>,
         val playerNumbers: List<Int>,
-        var cardScore: Int = 1
+        var cardScore: Int = 1,
+        var numberOfCards: Int = 1,
+        var winningNumbersCount: Int = 0
     )
 
     fun stringToInt(input: String): Int {
@@ -27,25 +29,26 @@ fun main() {
         val cardList = mutableListOf<Card>()
         for ((lineIndex, line) in input.withIndex()) {
             val card = parseCard(line, lineIndex)
-            println(card)
             var winningNumberCount = 0
             for (number in card.playerNumbers) {
                 if (card.winningNumbers.contains(number)) {
                     winningNumberCount++
                 }
-                card.cardScore = if (winningNumberCount == 0) {
-                    0
-                } else if (winningNumberCount == 1) {
-                    1
-                } else {
-                    2.0.pow(winningNumberCount - 1).toInt()
-                }
+            }
+            card.winningNumbersCount = winningNumberCount
+            card.cardScore = if (winningNumberCount == 0) {
+                0
+            } else if (winningNumberCount == 1) {
+                1
+            } else {
+                2.0.pow(winningNumberCount - 1).toInt()
             }
             cardList.add(card)
 
         }
         var total = 0
         for (card in cardList) {
+            println(card)
             total += card.cardScore
         }
         println("Total: $total")
@@ -53,13 +56,50 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val cardList = mutableListOf<Card>()
+        for ((lineIndex, line) in input.withIndex()) {
+            val card = parseCard(line, lineIndex)
+            var winningNumberCount = 0
+            for (number in card.playerNumbers) {
+                if (card.winningNumbers.contains(number)) {
+                    winningNumberCount++
+                }
+            }
+            card.winningNumbersCount = winningNumberCount
+            card.cardScore = if (winningNumberCount == 0) {
+                0
+            } else if (winningNumberCount == 1) {
+                1
+            } else {
+                2.0.pow(winningNumberCount - 1).toInt()
+            }
+            cardList.add(card)
+        }
+
+        for ((cardIndex, card) in cardList.withIndex()) {
+            var winningNumbers = card.winningNumbersCount
+            var currentIndex = cardIndex + 1
+            while (currentIndex < cardList.size && winningNumbers > 0) {
+                val nextCard = cardList[currentIndex]
+                nextCard.numberOfCards += (1 * card.numberOfCards)
+                winningNumbers--
+                currentIndex++
+            }
+        }
+
+        var total = 0
+        for (card in cardList) {
+            println("Card: $card")
+            total += card.numberOfCards
+        }
+        println("Total: $total")
+        return total
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 13)
-//    check(part2(testInput) == 1)
+    check(part2(testInput) == 30)
 
     val input = readInput("Day04")
     println(part1(input))
